a1a065c2790845da63205b7752e559402426714a
yichienlu <yichien.lu89@gmail.com>
2022-11-15 21:06:08 +0800

    Update 2022-11-15T13:06:07.455Z

---
+++ assets/js/all.js
@@ -0,0 +1,84 @@
+"use strict";
+
+// 畫面
+var menu = document.querySelector("#menu");
+
+function toggleMenu(state) {
+  if (state) {
+    menu.classList.remove("hidden");
+  } else {
+    menu.classList.add("hidden");
+  }
+} // 水平滾動
+
+
+(function () {
+  init();
+  var g_containerInViewport;
+
+  function init() {
+    setStickyContainersSize();
+    bindEvents();
+  }
+
+  function bindEvents() {
+    window.addEventListener("wheel", wheelHandler);
+  }
+
+  function setStickyContainersSize() {
+    document.querySelectorAll('.sticky-container').forEach(function (container) {
+      var stikyContainerHeight = container.querySelector('main').scrollWidth;
+      var stikyContainerHeight2 = container.querySelector('section.v').scrollHeight; // console.log(stikyContainerHeight, stikyContainerHeight2)
+
+      container.setAttribute('style', "height:".concat(stikyContainerHeight, "px")); //高度還要再試過
+    });
+  }
+
+  function isElementInViewport(el) {
+    var rect = el.getBoundingClientRect();
+    return rect.top <= 0 && rect.bottom > document.documentElement.clientHeight;
+  }
+
+  function wheelHandler(evt) {
+    var containerInViewPort = Array.from(document.querySelectorAll('.sticky-container')).filter(function (container) {
+      return isElementInViewport(container);
+    })[0];
+
+    if (!containerInViewPort) {
+      return;
+    }
+
+    var isPlaceHolderBelowTop = containerInViewPort.offsetTop < document.documentElement.scrollTop;
+    var isPlaceHolderBelowBottom = containerInViewPort.offsetTop + containerInViewPort.offsetHeight > document.documentElement.scrollTop;
+    var g_canScrollHorizontally = isPlaceHolderBelowTop && isPlaceHolderBelowBottom;
+
+    if (g_canScrollHorizontally) {
+      containerInViewPort.querySelector('main').scrollLeft += evt.deltaY;
+    }
+  }
+})(); //designs
+
+
+var swiper = new Swiper('.design-swiper', {
+  slidesPerView: 1,
+  spaceBetween: 16,
+  loop: true,
+  grabCursor: true,
+  speed: 2000,
+  // autoplay: {
+  //   delay: 2000,
+  //   disableOnInteraction: false,
+  //   pauseOnMouseEnter: true,
+  // },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 24
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 48
+    }
+  }
+});
+//# sourceMappingURL=all.js.map
