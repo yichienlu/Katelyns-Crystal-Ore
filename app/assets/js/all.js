70b063ddd9b9029d8ea73a3a952e763ef71b6dab
yichienlu <yichien.lu89@gmail.com>
2022-11-25 14:59:11 +0800

    20221125-02

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -13,13 +13,13 @@ let loggedin = JSON.parse(localStorage.getItem("token"));
 let isAdmin = JSON.parse(localStorage.getItem("isAdmin"));
 
 if(loggedin){
-  console.log(loggedin)
+  // console.log(loggedin)
   document.querySelector("#link_member").classList.remove("hidden")
   document.querySelector("#link_login").classList.add("hidden")
   document.querySelector("#link_logout").classList.remove("hidden")
 
   if(isAdmin){
-  console.log(isAdmin)
+  // console.log(isAdmin)
   document.querySelector("#link_admin").classList.remove("hidden")
 }
 }
