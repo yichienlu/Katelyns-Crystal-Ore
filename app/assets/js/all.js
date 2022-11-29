47daa4011fe6995c235dd54077e7f3c72e26540d
yichienlu <yichien.lu89@gmail.com>
2022-11-29 23:48:55 +0800

    20221129-03

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,7 +1,7 @@
 // 畫面
 const menu = document.querySelector("#menu");
-function toggleMenu(state){
-  if(state){
+function toggleMenu(state) {
+  if (state) {
     menu.classList.remove("hidden");
   } else {
     menu.classList.add("hidden");
@@ -13,22 +13,24 @@ let loggedin = JSON.parse(localStorage.getItem("token"));
 let isAdmin = JSON.parse(localStorage.getItem("isAdmin"));
 let userId = JSON.parse(localStorage.getItem("userId"));
 
-if(loggedin){
+if (loggedin) {
   // console.log(loggedin)
   document.querySelector("#link_member").classList.remove("hidden")
   document.querySelector("#link_login").classList.add("hidden")
   document.querySelector("#link_logout").classList.remove("hidden")
 
-  if(isAdmin){
-  // console.log(isAdmin)
-  document.querySelector("#link_admin").classList.remove("hidden")
-}
+  if (isAdmin) {
+    // console.log(isAdmin)
+    document.querySelector("#link_admin").classList.remove("hidden")
+  }
 }
 
-document.querySelector("#link_logout").addEventListener("click",function(){
+document.querySelector("#link_logout").addEventListener("click", function () {
   localStorage.removeItem("token")
   localStorage.removeItem("isAdmin")
   localStorage.removeItem("userId")
   alert("登出成功")
   window.location.assign("index.html");
 })
+
+
